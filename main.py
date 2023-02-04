eca9c7e4f9d400eab9976e92444678151a1a0444
Replit user <>
2023-02-04 01:23:57 +0000

    formatting the reponse from gpt-3

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def sentiment_analysis(texts):
     for text in texts:
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt='Sentiment analysis of the following text: ' + text + '\n' + '\n' + 'Label the sentiment as positive, neutral, or negative.',
+            prompt='Sentiment analysis of the following text: ' + text + '\n' + '\n' + 'Label the sentiment as positive, neutral, or negative. Do not provide any other feedback or words in your response.',
             max_tokens=1024,
             n=1,
             stop=None,
@@ -19,9 +19,9 @@ def sentiment_analysis(texts):
         sentiments.append(sentiment)
     return sentiments
 
-texts = ["This sample text is cool.",
-         "This sample text is a little sus.",
-        "This third sample text is ok."]
+texts = ["This sample text is sus.",
+         "This sample text is a little ok.",
+        "This third sample text is good."]
 sentiments = sentiment_analysis(texts)
 
 print(sentiments)
