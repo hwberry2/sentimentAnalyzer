7e297ceee6174841a0889a270b3ef4d49da09a26
Replit user <>
2023-02-04 00:48:05 +0000

    first commit from Replit

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import openai
 
-openai.api_key = "YOUR_API_KEY"
+#need to remove the key before pushing to GitHub
+openai.api_key = "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"
 
-def sentiment_analysis(text):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt='Sentiment analysis of the following text: ' + text + '\n' + '\n' + 'Label the sentiment as positive, neutral, or negative.',
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+def sentiment_analysis(texts):
+    sentiments = []
+    for text in texts:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt='Sentiment analysis of the following text: ' + text + '\n' + '\n' + 'Label the sentiment as positive, neutral, or negative.',
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        message = response["choices"][0]["text"].strip()
+        sentiment = message.split("\n")[-1].strip()
+        sentiments.append(sentiment)
+    return sentiments
 
-    message = response["choices"][0]["text"].strip()
-    sentiment = message.split("\n")[-1].strip()
+texts = ["This sample text is cool.",
+         "This sample text is a little sus.",
+        "This third sample text is ok."]
+sentiments = sentiment_analysis(texts)
 
-    return sentiment
+print(sentiments)
 
-text = "This is a sample text for sentiment analysis."
-sentiment = sentiment_analysis(text)
-print("Sentiment:", sentiment)
+for sentiment in sentiments:
+  print("Sentiments:", sentiment)
